757ab26261d9e65cf264c07893e61441cc7c4eb6
Kevin Muldoon <kmuldoon@mac.com>
2022-10-29 11:18:27 -0400

    Update build.js

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const config = require('./config/index.js')
 const brands = ['rj']
 // const platforms = ['js', 'css', 'scss', 'android', 'compose', 'objC', 'swift', 'swift_enum']
 const platforms = ['es6', 'es5', 'css']
+//
 
 brands.map(brand => {
     platforms.map(platform => {
